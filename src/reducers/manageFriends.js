eb7f64eda825757b3434e75ff3f007fdc8cb9a6f
cs-j <cj@mail.co.de>
2018-04-19 10:14:30 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      let friend = state.friends.find(friend => friend.id === action.id)
+      let updatedFriends = [...state.friends]
+      let index = updatedFriends.indexOf(friend)
+      updatedFriends.splice(index, 1)
+      return {friends: updatedFriends}
+    default:
+      return state;
+  }
 }
