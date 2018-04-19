94e80e27458237cd8904c7361367868572b5f55d
harenarium <22226614+harenarium@users.noreply.github.com>
2018-04-19 09:46:35 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      const index = state.friends.findIndex((friend) => friend.id === action.id);  //objects are not equal??
+      let newfriends = [
+        ...state.friends.slice(0, index),
+        ...state.friends.slice(index + 1)
+      ]
+      return {...state, friends: newfriends }
+    default:
+      return state
+  }
 }
